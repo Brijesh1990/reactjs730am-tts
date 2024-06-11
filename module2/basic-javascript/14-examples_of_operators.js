f0269d14766c8060b3937c69c2248d6604860459
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-06-11 08:57:41 +0530

    11-06-2024 all data uploaded

---
+++ module2/basic-javascript/14-examples_of_operators.js
@@ -0,0 +1,57 @@
+// let a=10;
+// let b=20;
+// let c=a+b;
+// console.log("additions of numbers is :",c);
+
+// type casting : we change any data type of any variables 
+// let a= parseInt("10"); //string
+// let b=20;
+// let c=a+b;
+// console.log("additions of numbers is :",c);
+
+// let a= parseFloat("10.456"); //string
+// let b=20;
+// let c=a+b;
+// console.log("additions of numbers is :",c);
+
+// let email='b@gmail.com';
+// let pass='b123456';
+// let res='b@gmail.com'?'You are Logged in successfully':'something went wrong';
+// console.log(res);
+
+
+// let age=18;
+// let res=age>18?'i am eligible for voting':'i am not eligible for voting';
+// console.log(res);
+
+
+// let a=10;
+// a++;
+// console.log(a);
+
+
+// let a=10;
+// console.log(a);
+// a++;
+
+// let a=10;
+// a--;
+// console.log(a);
+
+// let a=10;
+// a=a-1
+// console.log(a);
+
+// let a=10;
+// a=a+1 //a++
+// console.log(a);
+
+
+// let a=10;
+// let b=20;
+// a+=b; //a=a+b;
+// console.log(a);
+// let a=100;
+// let b=20;
+// a-=b; //a=a-b;
+// console.log(a);
---
+++ module2/basic-javascript/16-variables_check_datatypes.js
@@ -0,0 +1,5 @@
+// let a=15454545454n;
+// console.log(typeof(a));
+
+// let a=1545454545454545415454122121;
+// console.log(typeof(a));
