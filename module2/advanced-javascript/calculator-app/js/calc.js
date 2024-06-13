b7759f84116b7fbc8a8bcc97a0a7520323a081c5
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-06-13 09:11:10 +0530

    all data uploaded on 13-06-2024

---
+++ module2/advanced-javascript/calculator-app/js/calc.js
@@ -0,0 +1,31 @@
+// clear when click on C button
+function clr()
+{
+    document.getElementById("result").value="";
+}
+// when press on button its values get in result
+function getResult(val)
+{
+    document.getElementById("result").value+=val;
+}
+// when press on backspace its clear or slice one-one values
+function clrSlice()
+{
+
+}
+// get all airthmatic result
+function getFinalResult()
+{
+
+}
+// create a function for sqroot 
+function sqrtResult()
+{
+
+}
+// create a function for sqare 
+function sqrResult()
+{
+    
+}
+
