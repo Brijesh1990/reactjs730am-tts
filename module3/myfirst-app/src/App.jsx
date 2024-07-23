d1ebc7f01d2b4bb27a999bd39e974b06ddcf7c8a
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-07-23 11:36:29 +0530

    23-07-2024 all data uploaded

---
+++ module3/myfirst-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { version } from 'react';
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+      <h1>React js check version via vite :{version}</h1>
+      
+      </div>
+     
+    </>
+  )
+}
+
+export default App
